fc7f4f5946583d812e001d78a24ffb43146d6e93
Lux <lucaschcobarferreyra@gmail.com>
2023-11-11 18:07:19 -0300

    Modificación de los turnos

--- models/appointment.js
+++ models/appointment.js
@@ -3,31 +3,31 @@ const { Schema, model } = require("mongoose");
 const AppointmentSchema = Schema({
   detail: {
     type: String,
-    required: true,
+    required: [true, "El detalle es obligatorio"],
   },
   veterinarian: {
     type: String,
-    required: true,
+    required: [true, "El nombre del veterinario es obligatorio"],
   },
   pet: {
     type: String,
-    required: true,
+    required: [true, "El nombre de la mascota es obligatorio"],
   },
   date: {
     type: Date,
-    required: true,
-    default: Date.now,
+    required: [true, "La fecha es obligatoria"],
+    default: Date(),
   },
   state: {
     type: Boolean,
+    required: true,
     default: true,
-    required: true,
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+  // user: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "User",
+  //   required: true,
+  // },
 });
 
 module.exports = model("Appointment", AppointmentSchema);
